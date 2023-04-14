bad5db588480a85d11c63747b93c45f20ba8daeb
Robot Ecology Lab <robotecologylab@uci.edu>
2023-04-13 19:38:39 -0700

    Switched remote to UCI

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Use the detect_serial module to get the correct serial port
-docker run -d --restart always \
+docker run -d --restart unless-stopped \
 	--name firmware \
 	--net host \
 	--device $(sudo -u pi python3 ../gritsbot/detect_serial.py):/dev/ttyACM0 \
-	robotarium/firmware
+	robotecology/firmware
