f655c98e9db32a2a1fc2f887d01360613b73af3a
Robot Ecology Lab <robotecologylab@uci.edu>
2023-06-08 13:30:21 -0700

    fixed typos in docker_watch.ch

--- docker/docker_watch.sh
+++ docker/docker_watch.sh
@@ -5,6 +5,5 @@
 docker run -d \
 	--restart always \
 	--name watchtower \
-	--cleanup \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	v2tec/watchtower:armhf-latest -i 60 --debug
+	v2tec/watchtower:armhf-latest -i 60 --debug --cleanup
