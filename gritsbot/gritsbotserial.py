ec68fbfeebca6d9b321972f3002f0be1ae643e91
Soobum Kim <skim743@gatech.edu>
2023-08-03 13:57:25 -0400

    Testing

--- gritsbot/gritsbotserial.py
+++ gritsbot/gritsbotserial.py
@@ -56,7 +56,7 @@ class GritsbotSerial:
 
     """
 
-    def __init__(self, serial_dev='/dev/ttyAMA0', baud_rate=500000, timeout=None):
+    def __init__(self, serial_dev='/dev/ttyAMA0', baud_rate=500000, timeout=5):
         """Creates the serial communciations object.
 
         Args:
